8ba607427cd20ef97bd3466d7609b9f29ad6f5b1
hebertdtb777 <hebertdtb777@hotmail.com>
2015-07-16 09:36:07 -0500

    v3

--- js/juegojavascript.js
+++ js/juegojavascript.js
@@ -515,7 +515,106 @@ var game = (function () {
         }
     }
 
-  
+    /******************************* MEJORES PUNTUACIONES (LOCALSTORAGE) *******************************/
+    function saveFinalScore() {
+        localStorage.setItem(getFinalScoreDate(), getTotalScore());
+        showBestScores();
+        removeNoBestScores();
+    }
+
+    function getFinalScoreDate() {
+        var date = new Date();
+        return fillZero(date.getDay()+1)+'/'+
+            fillZero(date.getMonth()+1)+'/'+
+            date.getFullYear()+' '+
+            fillZero(date.getHours())+':'+
+            fillZero(date.getMinutes())+':'+
+            fillZero(date.getSeconds());
+    }
+
+    function fillZero(number) {
+        if (number < 10) {
+            return '0' + number;
+        }
+        return number;
+    }
+
+    function getBestScoreKeys() {
+        var bestScores = getAllScores();
+        bestScores.sort(function (a, b) {return b - a;});
+        bestScores = bestScores.slice(0, totalBestScoresToShow);
+        var bestScoreKeys = [];
+        for (var j = 0; j < bestScores.length; j++) {
+            var score = bestScores[j];
+            for (var i = 0; i < localStorage.length; i++) {
+                var key = localStorage.key(i);
+                if (parseInt(localStorage.getItem(key)) == score) {
+                    bestScoreKeys.push(key);
+                }
+            }
+        }
+        return bestScoreKeys.slice(0, totalBestScoresToShow);
+    }
+
+    function getAllScores() {
+        var all = [];
+        for (var i=0; i < localStorage.length; i++) {
+            all[i] = (localStorage.getItem(localStorage.key(i)));
+        }
+        return all;
+    }
+
+    function showBestScores() {
+        var bestScores = getBestScoreKeys();
+        var bestScoresList = document.getElementById('puntuaciones');
+        if (bestScoresList) {
+            clearList(bestScoresList);
+            for (var i=0; i < bestScores.length; i++) {
+                addListElement(bestScoresList, bestScores[i], i==0?'negrita':null);
+                addListElement(bestScoresList, localStorage.getItem(bestScores[i]), i==0?'negrita':null);
+            }
+        }
+    }
+
+    function clearList(list) {
+        list.innerHTML = '';
+        addListElement(list, "Fecha");
+        addListElement(list, "Puntos");
+    }
+
+    function addListElement(list, content, className) {
+        var element = document.createElement('li');
+        if (className) {
+            element.setAttribute("class", className);
+        }
+        element.innerHTML = content;
+        list.appendChild(element);
+    }
+
+    // extendemos el objeto array con un metodo "containsElement"
+    Array.prototype.containsElement = function(element) {
+        for (var i = 0; i < this.length; i++) {
+            if (this[i] == element) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    function removeNoBestScores() {
+        var scoresToRemove = [];
+        var bestScoreKeys = getBestScoreKeys();
+        for (var i=0; i < localStorage.length; i++) {
+            var key = localStorage.key(i);
+            if (!bestScoreKeys.containsElement(key)) {
+                scoresToRemove.push(key);
+            }
+        }
+        for (var j = 0; j < scoresToRemove.length; j++) {
+            var scoreToRemoveKey = scoresToRemove[j];
+            localStorage.removeItem(scoreToRemoveKey);
+        }
+    }
     /******************************* FIN MEJORES PUNTUACIONES *******************************/
 
     return {
